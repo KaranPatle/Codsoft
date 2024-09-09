94fef9b99bfc3a6dff503975fcc30f8b44ab8354
KaranPatle <karanp0401@gmail.com>
2024-09-09 16:22:55 +0530

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,47 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+  	double num1,num2;
+  	char op;
+  	
+  	cout<<"enter the operator(+ - * /) :";
+  	cin>>op;
+    cout<<"enter the two number one by one :";
+    cin>>num1>>num2;
+
+ switch (op)
+ {
+ 	case '+':
+ 		cout<<num1<<op<<num2<<"="<<num1+num2<<endl;
+ 		break;
+ 		
+ 		case '-':
+ 		cout<<num1<<op<<num2<<"="<<num1-num2<<endl;
+ 		break;
+ 		
+ 		case '*':
+ 		cout<<num1<<op<<num2<<"="<<num1*num2<<endl;
+ 		break;
+ 		
+ 		case '/':
+ 		if (num2==0.0)
+ 		{
+ 			cout<<"divide by zero situation"<<endl;
+ 			
+		 }
+		 else
+		 {
+		
+ 		cout<<num1<<op<<num2<<"="<<num1/num2<<endl;
+ 	
+		 }
+		 break;
+		 default:
+		 	cout<<"invalid operator"<<endl;     
+    }
+	 	
+		 	
+ 		
+ 	return 0;
+}    
